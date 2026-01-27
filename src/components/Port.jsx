9d7f56248b9867e61bdc1387c5956b40d1dc727d
wdz32e <tfdgp6v4qj@privaterelay.appleid.com>
2026-01-27 14:18:45 +0900

    port

--- src/components/Port.jsx
+++ src/components/Port.jsx
@@ -1,10 +1,113 @@
 import React from 'react'
+import Port1 from "../assets/img/port_1.png";
+import Port2 from "../assets/img/port_2.png";
+import Port3 from "../assets/img/port_3.png";
+import Port4 from "../assets/img/port_4.png";
+import Port5 from "../assets/img/port_5.png";
+import Port6 from "../assets/img/port_6.png";
+
 
 const Port = () => {
   return (
-    <div>
-        Port
-    </div>
+    <section id='port'>
+      <div className="port_inner">
+        <div className="port_title">
+          portfolio <em>작업물</em>
+        </div>
+        <div className="port_wrap">
+          <article className='port_item p1'>
+            <span className='num'>01.</span>
+            <a href="/" target='_blank' className='img'>
+                <img src={Port1} alt="포폴 이미지1" />
+            </a>
+            <h3 className='title'>A팀 프로젝트</h3>
+            <div className="desc">
+              1111 Lorem ipsum dolor sit, amet consectetur adipisicing elit. Recusandae eligendi exercitationem libero earum obcaecati, nulla molestias suscipit ad voluptates est aspernatur inventore molestiae cum vel optio, facere perferendis soluta dolorem.
+            </div>
+            <a href="/" target='_blank' className='site' rel='noreferrer noopener'>사이트 보기</a>
+
+            {/* noreferrer : 정보보안용 => 사용자가 링크를 클릭했을 때 어디서 왔는지 정보가 전달되게 해주는 경로를 숨겨줌 */}
+            {/* noopener :  보인용 => 윈도우 차단, 해킹, 피싱 등을 방지해줌*/}
+          </article>
+
+          <article className='port_item p2'>
+            <span className='num'>02.</span>
+            <a href="/" target='_blank' className='img'>
+                <img src={Port2} alt="포폴 이미지2" />
+            </a>
+            <h3 className='title'>B팀 프로젝트</h3>
+            <div className="desc">
+              2222 Lorem ipsum dolor sit, amet consectetur adipisicing elit. Recusandae eligendi exercitationem libero earum obcaecati, nulla molestias suscipit ad voluptates est aspernatur inventore molestiae cum vel optio, facere perferendis soluta dolorem.
+            </div>
+            <a href="/" target='_blank' className='site' rel='noreferrer noopener'>사이트 보기</a>
+
+            {/* noreferrer : 정보보안용 => 사용자가 링크를 클릭했을 때 어디서 왔는지 정보가 전달되게 해주는 경로를 숨겨줌 */}
+            {/* noopener :  보인용 => 윈도우 차단, 해킹, 피싱 등을 방지해줌*/}
+          </article>
+
+          <article className='port_item p3'>
+            <span className='num'>03.</span>
+            <a href="/" target='_blank' className='img'>
+                <img src={Port3} alt="포폴 이미지3" />
+            </a>
+            <h3 className='title'>C팀 프로젝트</h3>
+            <div className="desc">
+              3333 Lorem ipsum dolor sit, amet consectetur adipisicing elit. Recusandae eligendi exercitationem libero earum obcaecati, nulla molestias suscipit ad voluptates est aspernatur inventore molestiae cum vel optio, facere perferendis soluta dolorem.
+            </div>
+            <a href="/" target='_blank' className='site' rel='noreferrer noopener'>사이트 보기</a>
+
+            {/* noreferrer : 정보보안용 => 사용자가 링크를 클릭했을 때 어디서 왔는지 정보가 전달되게 해주는 경로를 숨겨줌 */}
+            {/* noopener :  보인용 => 윈도우 차단, 해킹, 피싱 등을 방지해줌*/}
+          </article>
+
+          <article className='port_item p4'>
+            <span className='num'>04.</span>
+            <a href="/" target='_blank' className='img'>
+                <img src={Port4} alt="포폴 이미지4" />
+            </a>
+            <h3 className='title'>D팀 프로젝트</h3>
+            <div className="desc">
+              4444 Lorem ipsum dolor sit, amet consectetur adipisicing elit. Recusandae eligendi exercitationem libero earum obcaecati, nulla molestias suscipit ad voluptates est aspernatur inventore molestiae cum vel optio, facere perferendis soluta dolorem.
+            </div>
+            <a href="/" target='_blank' className='site' rel='noreferrer noopener'>사이트 보기</a>
+
+            {/* noreferrer : 정보보안용 => 사용자가 링크를 클릭했을 때 어디서 왔는지 정보가 전달되게 해주는 경로를 숨겨줌 */}
+            {/* noopener :  보인용 => 윈도우 차단, 해킹, 피싱 등을 방지해줌*/}
+          </article>
+
+          <article className='port_item p5'>
+            <span className='num'>05.</span>
+            <a href="/" target='_blank' className='img'>
+                <img src={Port5} alt="포폴 이미지5" />
+            </a>
+            <h3 className='title'>E팀 프로젝트</h3>
+            <div className="desc">
+              5555 Lorem ipsum dolor sit, amet consectetur adipisicing elit. Recusandae eligendi exercitationem libero earum obcaecati, nulla molestias suscipit ad voluptates est aspernatur inventore molestiae cum vel optio, facere perferendis soluta dolorem.
+            </div>
+            <a href="/" target='_blank' className='site' rel='noreferrer noopener'>사이트 보기</a>
+
+            {/* noreferrer : 정보보안용 => 사용자가 링크를 클릭했을 때 어디서 왔는지 정보가 전달되게 해주는 경로를 숨겨줌 */}
+            {/* noopener :  보인용 => 윈도우 차단, 해킹, 피싱 등을 방지해줌*/}
+          </article>
+          
+          <article className='port_item p6'>
+            <span className='num'>06.</span>
+            <a href="/" target='_blank' className='img'>
+                <img src={Port6} alt="포폴 이미지6" />
+            </a>
+            <h3 className='title'>F팀 프로젝트</h3>
+            <div className="desc">
+              6666 Lorem ipsum dolor sit, amet consectetur adipisicing elit. Recusandae eligendi exercitationem libero earum obcaecati, nulla molestias suscipit ad voluptates est aspernatur inventore molestiae cum vel optio, facere perferendis soluta dolorem.
+            </div>
+            <a href="/" target='_blank' className='site' rel='noreferrer noopener'>사이트 보기</a>
+
+            {/* noreferrer : 정보보안용 => 사용자가 링크를 클릭했을 때 어디서 왔는지 정보가 전달되게 해주는 경로를 숨겨줌 */}
+            {/* noopener :  보인용 => 윈도우 차단, 해킹, 피싱 등을 방지해줌*/}
+          </article>
+
+        </div>
+      </div>
+    </section>
   )
 }
 
--- src/components/Site.jsx
+++ src/components/Site.jsx
@@ -1,10 +1,78 @@
 import React from 'react'
 
+const siteText =[
+  {
+    text:["Make"," site compliant with", "mbc academy"],
+    title:"웹표준을 준수한 사이트 제작",
+    code:"/",
+    view:"/",
+    infor:["site coding","roduction period:two days","html5, css3, script" ],
+  },
+  {
+    text:["Make"," site compliant with", "1차 포트폴리오"],
+    title:"1차 포트폴리오 사이트 제작",
+    code:"/",
+    view:"/",
+    infor:["site coding","roduction period:two days","html5, css3, script" ],
+  },
+  {
+    text:["Make"," site compliant with", "팀프로젝트"],
+    title:"팀프로젝트 사이트 제작",
+    code:"/",
+    view:"/",
+    infor:["site coding","roduction period:two days","html5, css3, script" ],
+  },
+    {
+    text:["Make"," site compliant with", "2차 포트폴리오"],
+    title:"2차 포트폴리오 사이트 제작",
+    code:"/",
+    view:"/",
+    infor:["site coding","roduction period:two days","html5, css3, script" ],
+  },
+      {
+    text:["Make"," site compliant with", "2차 포트폴리오"],
+    title:"2차 포트폴리오 사이트 제작",
+    code:"/",
+    view:"/",
+    infor:["site coding","roduction period:two days","html5, css3, script" ],
+  },
+
+]
+
+
+
 const Site = () => {
   return (
-    <div>
-        Site
-    </div>
+    <section id='site'>
+      <div className="site_inner">
+        <h2 className='site_title'>Site coding <em>나의 작업물</em></h2>
+        <section className='site_wrap'>
+            {siteText.map((site,key)=>(
+              <article className={`site_item s${key+1}`} key={key}>
+                {/* 회전할 아이 =>배경색 뺵팁은 계산식이 있을때*/ }
+                <span className='num'>{key+1}.</span>{/* 회전할 아이=>숫자 */}
+                <div className="text">
+                    <div>{site.text[0]}</div>
+                    <div>{site.text[1]}</div>
+                    <div>{site.text[2]}</div>{/* =>회전할 아이 length3 */}
+                </div>
+                <h3 className='title'>{site.title}</h3>
+                <div className="btn">
+                  <a href={site.code}>code</a>
+                  <a href={site.view}>view</a>
+                </div>
+                <div className="infor">
+                  <span>{site.infor[0]}</span>
+                  <span>{site.infor[1]}</span>
+                  <span>{site.infor[2]}</span>
+                </div>
+              </article>
+            ))}
+            {/* siteText=site */}
+
+        </section>
+      </div>
+    </section> 
   )
 }
 
